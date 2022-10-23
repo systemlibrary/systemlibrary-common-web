c0566a3b2eca73887f00317220287381ab6e7c1b
systemlibrary <ceo@systemlibrary.com>
2022-10-24 01:41:41 +0200

    Release 6.2.1.4 - Added multiple media types by default - Updated deps and docs

--- README.md
+++ README.md
@@ -28,9 +28,9 @@ The two methods in short enables:
 - Microsoft.Extensions.FileProviders.Physical &gt;= 6.0.0
 
 ## Latest Version
-- 6.2.1.3
+- 6.2.1.4
+- Added audio mediatypes, video/ogg and a few more default media types to be served "out of the box"
 - Updated deps 
-- Updated 'deps' minimum version to 1000 in csproj - as most packages should just always work although the "exact version" as the "exact version" in csproj works diff than nuspec (?)
 
 #### Version history
 - View git history of this file if interested
--- source/SystemLibrary.Common.Web/DefaultSupportedMediaTypes/DefaultSupportedMediaTypes.cs
+++ source/SystemLibrary.Common.Web/DefaultSupportedMediaTypes/DefaultSupportedMediaTypes.cs
@@ -6,24 +6,11 @@ internal class DefaultSupportedMediaTypes : StringOutputFormatter
 {
     internal DefaultSupportedMediaTypes()
     {
-        SupportedMediaTypes.Add("text/html");
-        SupportedMediaTypes.Add("text/css");
-        SupportedMediaTypes.Add("text/csv");
-        SupportedMediaTypes.Add("text/plain");
-        SupportedMediaTypes.Add("text/javascript");
-
-        SupportedMediaTypes.Add("font/ttf");
-        SupportedMediaTypes.Add("font/otf");
-        SupportedMediaTypes.Add("font/woff2");
-        SupportedMediaTypes.Add("font/woff");
-
-        SupportedMediaTypes.Add("image/svg+xml");
-        SupportedMediaTypes.Add("image/jpeg");
-        SupportedMediaTypes.Add("image/gif");
-        SupportedMediaTypes.Add("image/png");
-        SupportedMediaTypes.Add("image/tiff");
-        SupportedMediaTypes.Add("image/webp");
-        SupportedMediaTypes.Add("image/x-icon");
+        SupportedMediaTypes.Add("audio/mpeg");
+        SupportedMediaTypes.Add("audio/wave");
+        SupportedMediaTypes.Add("audio/wav");
+        SupportedMediaTypes.Add("audio/webm");
+        SupportedMediaTypes.Add("audio/ogg");
 
         SupportedMediaTypes.Add("application/x-font-ttf");
         SupportedMediaTypes.Add("application/x-font-opentype");
@@ -33,10 +20,36 @@ internal class DefaultSupportedMediaTypes : StringOutputFormatter
         SupportedMediaTypes.Add("application/gzip");
         SupportedMediaTypes.Add("application/pdf");
         SupportedMediaTypes.Add("application/json");
+        SupportedMediaTypes.Add("application/pkcs8");
         SupportedMediaTypes.Add("application/rss+xml");
+
+        SupportedMediaTypes.Add("font/ttf");
+        SupportedMediaTypes.Add("font/otf");
+        SupportedMediaTypes.Add("font/woff2");
+        SupportedMediaTypes.Add("font/woff");
+
+        SupportedMediaTypes.Add("multipart/form-data");
+        SupportedMediaTypes.Add("multipart/byteranges");
+
+        SupportedMediaTypes.Add("image/svg+xml");
+        SupportedMediaTypes.Add("image/apng");
+        SupportedMediaTypes.Add("image/jpeg");
+        SupportedMediaTypes.Add("image/gif");
+        SupportedMediaTypes.Add("image/png");
+        SupportedMediaTypes.Add("image/tiff");
+        SupportedMediaTypes.Add("image/webp");
+        SupportedMediaTypes.Add("image/x-icon");
+        SupportedMediaTypes.Add("image/avif");
+
+        SupportedMediaTypes.Add("text/html");
+        SupportedMediaTypes.Add("text/css");
+        SupportedMediaTypes.Add("text/csv");
+        SupportedMediaTypes.Add("text/plain");
+        SupportedMediaTypes.Add("text/javascript");
 
         SupportedMediaTypes.Add("video/mp4");
         SupportedMediaTypes.Add("video/m4v");
         SupportedMediaTypes.Add("video/webm");
+        SupportedMediaTypes.Add("video/ogg");
     }
 }
