3b843a8b2db096067b979677674b393f01c42df8
systemlibrary <ceo@systemlibrary.com>
2023-03-29 00:38:28 +0200

    Release 6.7.2.1 - Referer and IsAjaxRequest HttpRequestExtensions

--- README.md
+++ README.md
@@ -28,8 +28,8 @@ The two methods in short enables:
 - Microsoft.Extensions.FileProviders.Physical &gt;= 6.0.0
 
 ## Latest Version
-- 6.7.1.1
-- Updated deps of SystemLibrary.Common.Net to 6.7.*
+- 6.7.2.1
+- HttpRequest extensions added: Referer() and IsAjaxRequest()
 
 
 #### Version history
--- source/SystemLibrary.Common.Web/Extensions/HttpRequestExtensions/HttpRequestExtensions.cs
+++ source/SystemLibrary.Common.Web/Extensions/HttpRequestExtensions/HttpRequestExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Headers;
 
 namespace SystemLibrary.Common.Web.Extensions;
 
@@ -9,6 +12,26 @@ public static class HttpRequestExtensions
         if (request == null) return null;
 
         return request.Scheme + "://" + request.Host + request.Path + request.QueryString.Value;
+    }
+
+    public static bool IsAjaxRequest(this HttpRequest request)
+    {
+        if (request == null)
+            throw new ArgumentNullException(nameof(request));
+
+        if (request.Headers != null && request.Headers.ContainsKey("X-Requested-With"))
+            return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+
+        return false;
+    }
+
+    public static Uri Referer(this HttpRequest request)
+    {
+        if(request == null) throw new ArgumentNullException(nameof(request));
+
+        RequestHeaders header = request.GetTypedHeaders();
+
+        return header?.Referer;
     }
 }
 
