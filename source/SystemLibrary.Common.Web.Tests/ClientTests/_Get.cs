f04a69c29da7a854a3b8941eec09f420ae174c93
ceo <ceo@systemlibrary.com>
2024-10-24 12:18:49 +0200

    - 7.8.2.2 - ClientResponse now returns Data from ClientResponse&lt;T&gt; if it is set, so serializing a ClientResponse.Data is now doable without cast (fix)

--- source/SystemLibrary.Common.Web.Tests/ClientTests/_Get.cs
+++ source/SystemLibrary.Common.Web.Tests/ClientTests/_Get.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using SystemLibrary.Common.Net.Extensions;
+
 namespace SystemLibrary.Common.Web.Tests;
 
 partial class ClientTests
@@ -12,5 +14,21 @@ partial class ClientTests
 
         Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
         Assert.IsTrue(response.Data.Contains("httpbin.org"));
+    }
+
+    [TestMethod]
+    public void Get_Client_Response_Serialize_Data_Success()
+    {
+        var bin = new HttpBin();
+        var response = bin.Get();
+
+        Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+
+        var clientResponse = response as ClientResponse;
+
+        var data = clientResponse.Data;
+
+        Assert.IsTrue(data != null, "Data is null, interface Data explicit implementation changed");
+        Assert.IsTrue(data.Json().Length > 10, "Data is too short after serialization");
     }
 }
--- source/SystemLibrary.Common.Web/Client/Response/ClientResponse.cs
+++ source/SystemLibrary.Common.Web/Client/Response/ClientResponse.cs
@@ -1,90 +1,34 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Text.Json.Serialization;
 
 namespace SystemLibrary.Common.Web;
 
 /// <summary>
-/// The response that all Client methods returns
-/// <para>Note that a response statuscode might be 200 OK, but the IsSuccess might be false in scenarios where no response were returned</para>
-/// </summary>
-/// <typeparam name="T">T can be a string that you later can convert to json, or it can be a class, or a list of a class that will automatically be converted from json, assuming json response</typeparam>
-public partial class ClientResponse<T> : ClientResponse, IDisposable
-{
-    internal ClientResponse(HttpResponseMessage response, T data, string reason = null)
-    {
-        Response = response;
-        Data = data;
-        Reason = reason;
-        if (reason != null)
-        {
-            if (response == null)
-            {
-                if (reason.StartsWith("No such host"))
-                    Code = (HttpStatusCode)599;
-                else
-                    Code = HttpStatusCode.BadGateway;
-            }
-        }
-    }
-
-    /// <summary>
-    /// Pass in data that is set on as the 'data' with a response of status code 500
-    /// <para>Use in your own 'BaseClient' if you want to sent some specific message or data upwards</para>
-    /// </summary>
-    public ClientResponse(T defaultData = default)
-    {
-        Data = defaultData;
-        Response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
-        Response.ReasonPhrase = HttpStatusCode.InternalServerError.ToString();
-    }
-
-    /// <summary>
-    /// Pass in data that is set on as the 'data' with a response with a status code of your own choice
-    /// </summary>
-    public ClientResponse(T defaultData, HttpStatusCode statusCode)
-    {
-        Data = defaultData;
-        Response = new HttpResponseMessage(statusCode);
-        Response.ReasonPhrase = statusCode.ToString();
-    }
-
-    [JsonIgnore]
-    string Reason;
-
-    [JsonIgnore]
-    HttpStatusCode Code = HttpStatusCode.OK;
-
-    public T Data { get; }
-
-    public HttpStatusCode StatusCode => Response?.StatusCode ?? Code;
-    public string Message => Response?.ReasonPhrase ?? Reason;
-
-    public void Dispose()
-    {
-        if (Response != null)
-        {
-            try
-            {
-                Response.Dispose();
-            }
-            catch
-            {
-            }
-        }
-    }
-}
-
-/// <summary>
 /// Base class of a ClientResponse 
 /// <para>- Contains the HttpResponseMessage itself </para>
-/// Used when you do not want to return 'object' nor generic type, but you want to be somewhat clear in what object is returned
+/// Used when you do not want to return 'object' nor generic type, but you want to be clear in what object is returned in your C# functions
+/// <para>- Contains 'data' variable for serializing purposes so it is not undefined</para>
 /// </summary>
 public class ClientResponse
 {
     [JsonIgnore]
     public HttpResponseMessage Response { get; internal set; }
+
     public bool IsSuccess => Response?.IsSuccessStatusCode == true;
 
+    /// <summary>
+    /// NOTE: Variable is always null, it exists so serializing data will not give 'undefined' in javascript-world
+    /// </summary>
+    public object Data
+    {
+        get
+        {
+            if (this is IClientResponse i)
+            {
+                return i.Data;
+            }
+
+            return null;
+        }
+    }
 }
